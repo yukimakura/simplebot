3f56e3433211ef57e2b1f31ed8dcae9bd524bb43
yukimakura <yukimakura86@gmail.com>
2021-01-04 22:28:37 +0900

    hardwareinterface追加（実際の通信部分は未実装）

---
+++ simplebot_hardware/src/simplebot_hardware_node.cpp
@@ -0,0 +1,24 @@
+#include "simplebot_hardware/simplebot_hardware.hpp"
+#include "controller_manager/controller_manager.h"
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "simplebot_hardware_node");
+  ros::NodeHandle nh;
+
+  simplebotHW robot;
+  controller_manager::ControllerManager cm(&robot, nh);
+
+  ros::Rate rate(1.0 / robot.getPeriod().toSec());
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  while(ros::ok()){
+    robot.read(robot.getTime(), robot.getPeriod());
+    cm.update(robot.getTime(), robot.getPeriod());
+    robot.write(robot.getTime(), robot.getPeriod());
+    rate.sleep();
+  }
+  spinner.stop();
+
+  return 0;
+}
